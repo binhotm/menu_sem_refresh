80108898470709e009bfc9e102703587f33d7c27
Raphael BErtani <shark3ds@gmail.com>
2014-12-27 22:04:19 -0200

    help2

--- fotosx.php
+++ fotosx.php
@@ -1,29 +1,21 @@
  
-<div id="content" onload='load_video()'>
+<div id="content" onended="load_video()">
 
 
     <!--SVP embed code begin-->
     <div  id="svp_playered2l3yzlr2o8" style="width:600px;height:338px;position:relative;">
         <a class="svp_embed_link" style="color:#000;cursor:default;" 
            href="http://www.streamingvideoprovider.com/pay-per-view-access-restrictions.html" 
-           title="pay best per view video streaming service" rel="nofollow">
+           title="pay best per view video streaming service" rel="nofollow" onended="load_video()">
             per view video streaming  by StreamingVideoProvider</a>
     </div>
     <script language="javascript" type="text/javascript" src="dplayer.js"></script>
 
-    <script>
-    $(document).ready(function () {
 
-        alert('script inicial');
-        var vars = {clip_id: "ed2l3yzlr2o8", transparent: "true", pause: "1", repeat: "", bg_color: "#FFFFFF", fs_mode: "2", no_controls: "", start_img: "1", start_volume: "100", close_button: "", brand_new_window: "1", auto_hide: "1", stretch_video: "", player_align: "NONE", offset_x: "", offset_y: "", player_color_ratio: 0.6, skinAlpha: "80", colorBase: "#202020", colorIcon: "#FFFFFF", colorHighlight: "#fcad37", direct: "false", is_responsive: "false", viewers_limit: 0, cc_position: "bottom", cc_positionOffset: 70, cc_multiplier: 0.03, cc_textColor: "#ffffff", cc_textOutlineColor: "#000000", cc_bkgColor: "#000000", cc_bkgAlpha: 0.7};
-        var svp_player = new SVPDynamicPlayer("svp_playered2l3yzlr2o8", "", "600", "338", {use_div: "svp_playered2l3yzlr2o8", skin: "3"}, vars);
-        svp_player.execute();
-        return false;
-    });
-    
-    </script>
+
     <!--SVP embed code end-->
     <input type="button" onclick="set_flag()" value="Atualizar Menu Flag"/>
-    <input type="button" onclick="load_video()" value="Carregar Vídeo"/>
+    <input type="button" onclick="load_video('ed2l3yzlr2o8')" value="Carregar Vídeo"/>
 
+  
 </div><!-- /content -->
--- menu.php
+++ menu.php
@@ -1,50 +1,28 @@
-<!doctype html>
-<html lang=''>
-    <head>
-        <meta charset='utf-8'>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
-        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-        <script src="script.js"></script>
-        <title>CSS MenuMaker</title>
-    </head>
-    <body>
 
         <div id="cssmenu">
             <ul>
                 <li class='active'><a href='index.php'><span>Abertura</span></a></li>
-
                 <li class='has-sub'><a href='#'><span>Modulo I</span></a>
-
                     <ul>
-
                         <li class='has-sub'><a href='fotosx.php'><span>Instalação</span></a>
                             <ul>
                                 <li><a href='fotosx.php'>
                                         <span id="monange">
                                             Minisap<img src="ok.png" height="15" width="15" style="margin-left:10px;">
                                         </span>
-
                                     </a>
                                 </li>
-
                                 <li class='last'><a href='#'><span>Configuração</span></a></li>
-
                             </ul>
                         </li>
-
                         <li class='has-sub'><a href='#'><span>Execução</span></a>
                             <ul>
                                 <li><a href='#'><span>Blasters I</span></a></li>
                                 <li class='last'><a href='#'><span>Blasters II</span></a></li>
                             </ul>
-
                         </li> 
                     </ul>
-
                 </li>
-
                 <li class='has-sub'><a href='#'><span>Modulo II</span></a>
                     <ul>
                         <li class='has-sub'><a href='#'><span>Instalação</span></a>
@@ -62,7 +40,6 @@
                         </li>
                     </ul>
                 </li>
-
                 <li class='has-sub'><a href='#'><span>Modulo III</span></a>
                     <ul>
                         <li class='has-sub'><a href='#'><span>Instalação</span></a>
@@ -79,10 +56,6 @@
                         </li>
                     </ul>
                 </li>
-
                 <li><a href='#'><span>Sair</span></a></li>
-
-            </ul></div>
-
-    </body>
-    <html>
+            </ul>
+        </div>
